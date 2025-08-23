5334cc227b0148ef5f692ffc16ab432159c79d7c
navaalnoshi <navaalnoshi18@gmail.com>
2025-08-23 21:41:13 +0500

    Create huff_constants.svh

---
+++ rtl/huff_constants.svh
@@ -0,0 +1,18 @@
+// Copyright 2025 Maktab-e-Digital Systems Lahore.
+// Licensed under the Apache License, Version 2.0, see LICENSE file for details.
+// SPDX-License-Identifier: Apache-2.0
+//
+// Description:
+//    Header file defining localparams for JPEG Huffman encoding.
+//    - DC Huffman code lengths and codes (Luma & Chroma)
+//    - AC Huffman code lengths and codes (Luma & Chroma)
+//    - Run-length/size to Huffman index mapping
+//
+// Author: Rameen
+// Date  : 23rd July 2025
+
+`ifndef HUFF_CONSTANTS_SVH
+`define HUFF_CONSTANTS_SVH
+
+
+`endif // HUFF_CONSTANTS_SVH
